3de89313189099ca61657a37751a968c9d9b0e2d
Haris Bin Amir <hba777@outlook.com>
2024-10-11 18:49:18 +0500

    Updated Origin

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors(
   {
-    origin: "",
+    origin: "https://food-website-frontend-chi.vercel.app/",
     methods: ["POST", "GET"],
     credentials: true
   }
